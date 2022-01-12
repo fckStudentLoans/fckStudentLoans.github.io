88a4d8c6b88ec89905f4432df37858e9325ce894
fckStudentLoans <95000729+fckStudentLoans@users.noreply.github.com>
2022-01-11 22:39:06 -0800

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 window.onload = function () {
-    jQuery("#numSubs").html('10');
-    jQuery('.box .percent svg circle:nth-child(2)').css('stroke-dashoffset','calc(440 - (440 * 10) / 100)');
+    jQuery("#numSubs").html('14');
+    jQuery('.box .percent svg circle:nth-child(2)').css('stroke-dashoffset','calc(440 - (440 * 14) / 100)');
  }
             
         
