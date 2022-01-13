3e3304077090004e40f4f853276dce3a5a2a8b36
fckStudentLoans <95000729+fckStudentLoans@users.noreply.github.com>
2022-01-13 09:47:13 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 window.onload = function () {
-    jQuery("#numSubs").html('17');
-    jQuery('.box .percent svg circle:nth-child(2)').css('stroke-dashoffset','calc(440 - (440 * 17) / 100)');
+    jQuery("#numSubs").html('23');
+    jQuery('.box .percent svg circle:nth-child(2)').css('stroke-dashoffset','calc(440 - (440 * 23) / 100)');
  }
             
         
